91d3d143350597dae7d3c27e8b4ebc77863a40f7
DastaverGit <dastaver7@gmail.com>
2026-02-14 10:15:25 +0000

    Exercicios

---
+++ Exercicios/aula012/ex011.js
@@ -0,0 +1,8 @@
+var idade = 22
+if (idade) {
+    console.log('Não vota')
+}else if(idade >= 18){
+    console.log('Voto opcional')
+}else if ('idade >=18'){
+    console.log ('Voto Obrigatório')
+}
