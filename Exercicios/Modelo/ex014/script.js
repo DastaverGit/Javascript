bfad93eac77bb2709b9908a596a3c0cb62c706c7
DastaverGit <dastaver7@gmail.com>
2026-02-16 07:56:11 +0000

    Continuação

--- Exercicios/Modelo/ex014/script.js
+++ Exercicios/Modelo/ex014/script.js
@@ -1,2 +1,18 @@
-alert('Olá')
- 
+function carregar () {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 20
+    msg.innerHTML=`Agora são ${hora} horas.`
+     if(hora >= 0 && hora < 12) {
+        //Bom dia
+        img = src `imagem/Manha.jpg`
+     }else if (hora >= 12 && hora <= 18) {
+        //Boa Tarde
+        img = src `imgem/Dia.jpg`
+     }else {
+        //Boa noite
+        img = src `imagem/Noite.jpg`
+     }
+}
