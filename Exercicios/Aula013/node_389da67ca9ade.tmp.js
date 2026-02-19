ac68d2def794e9c0ff33f5ac2298273eae5a07d6
DastaverGit <dastaver7@gmail.com>
2026-02-19 18:48:41 +0000

    Introduções de aula de Repitições!

---
+++ Exercicios/Aula013/node_389da67ca9ade.tmp.js
@@ -0,0 +1,16 @@
+var c =1
+while( c <= 10000000000000) {
+    console.log('Tudo bem')
+    c++ 
+}
+
+
+
+/*
+console.log('Tudo bemm')
+console.log('Tudo bemm')
+console.log('Tudo bemm')
+console.log('Tudo bemm')
+console.log('Tudo bemm')
+console.log('Tudo bemm')
+*/
