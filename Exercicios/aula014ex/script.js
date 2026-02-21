c01c306d919dcd1411a1f71ddab312d7624a035a
DastaverGit <dastaver7@gmail.com>
2026-02-21 07:50:38 +0000

    Continuação

---
+++ Exercicios/aula014ex/script.js
@@ -0,0 +1,29 @@
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if (ini.value.length==0 || fim.value.length==0 || passo.value.length==0){
+        window.alert('[ERRO] Faltam dados')
+    }else{
+       res.innerHTML = 'Contando:' 
+       let i = Number(ini.value)
+       let f = Number (fim.value)
+       let p = Number (passo.value)
+        if  (i < f) {
+            //contagem crescente
+           for(let c= 1; c <= f; c+= p){
+        }
+        res.innerHTML+= `\u[🍁]` 
+        }else{
+            //contagem regressiva
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += `${c}🍁`
+            }
+           res.innerHTML +=  `${c}👉`
+        }
+        
+    }
+}
+ 
