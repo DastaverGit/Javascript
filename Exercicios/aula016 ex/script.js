928688a1e861edff3e504054cde9b83aa357022f
DastaverGit <dastaver7@gmail.com>
2026-02-28 08:20:51 +0000

    Finalizando o projeto!

--- Exercicios/aula016 ex/script.js
+++ Exercicios/aula016 ex/script.js
@@ -3,7 +3,7 @@ let lista = document.querySelector('select#flista')
 let res = document.querySelector('div#res')
 let valores = []
 
-function isNumero(n){
+function IsNumero(n){
     if(Number(n)>=1 && Number(n)<=100){
         return true
     }else{
@@ -11,22 +11,37 @@ function isNumero(n){
     }
 }
 
-function inLista(n, i){
-    if(i.indexOf(Numero(n)) |=-1){
+
+
+function isNumero(n){
+    if(Number(n) >= 1 && Number(n) <= 100){
         return true
-    }else{
+    } else {
         return false
     }
 }
 
+function inLista(n, l){ // mudei 'i' para 'l' (lista) para ficar mais claro
+    if(l.indexOf(Number(n)) != -1){ // Corrigido: Number e != -1
+        return true
+    } else {
+        return false
+    }
+}
 
 function adicionar(){
-    if(isNumero(num.value) && inLista(num.value, valores)){
+    // Adicionei o ! antes de inLista para significar "NÃO está na lista"
+    if(isNumero(num.value) && !inLista(num.value, valores)){
         valores.push(Number(num.value))
         let item = document.createElement('option')
         item.text = `valor ${num.value} adicionado.`
         lista.appendChild(item)
-    }else{
-        window.alert('Valor invalido ou já encontrado na  lista.')
+        res.innerHTML = '' // Limpa o resultado quando adiciona um novo
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista.')
     }
+    
+    // Dica extra: Limpa o campo e foca o cursor automaticamente
+    num.value = ''
+    num.focus()
 }
